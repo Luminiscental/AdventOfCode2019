6c5f86af42b0e677d2f63de7b4729533955d6df3
Luminiscental <luminiscental01@gmail.com>
2019-12-01 17:34:49 +0000

    Add some error handling to util.py

--- util.py
+++ util.py
@@ -2,6 +2,7 @@
 General utility functions for all days.
 """
 
+import sys
 import requests
 
 
@@ -9,9 +10,16 @@ def get_input(day_number):
     """
     Get the puzzle input for the given day.
     """
-    with open(".cookie", "r") as cookie_file:
-        cookie = cookie_file.read().strip()
-        return requests.get(
-            f"https://adventofcode.com/2019/day/{day_number}/input",
-            cookies={"session": cookie},
-        ).text
+    try:
+        with open(".cookie", "r") as cookie_file:
+            cookie = cookie_file.read().strip()
+            return requests.get(
+                f"https://adventofcode.com/2019/day/{day_number}/input",
+                cookies={"session": cookie},
+            ).text
+    except FileNotFoundError:
+        print(
+            "Could not find .cookie file to get input from adventofcode.com, "
+            "input is user-dependent so I need your session id"
+        )
+        sys.exit(1)
