4dd8a371af53ac30d59403eb67a3ffa4cdacdee2
Luminiscental <luminiscental01@gmail.com>
2019-12-21 10:52:53 +0000

    Solve day 21 part 2

--- day21.py
+++ day21.py
@@ -7,39 +7,51 @@ from day02 import parse
 class Droid:
     """Class to handle the springdroid."""
 
-    def __init__(self, program, run=False):
+    def __init__(self, program):
         self.interpretor = intcode.Interpretor()
         self.program = program
-        self.run = run
 
-    def exec(self, script, debug=False):
+    def exec(self, script, run=False, debug=False):
         """Execute a script, given as a sequence of instructions."""
         for instr in script:
             self.interpretor.queue_inputs(map(ord, instr))
             self.interpretor.queue_input(ord("\n"))
-        self.interpretor.queue_inputs(map(ord, "RUN\n" if self.run else "WALK\n"))
+        self.interpretor.queue_inputs(map(ord, "RUN\n" if run else "WALK\n"))
         if debug:
             for out in self.interpretor.run(self.program):
-                print(out, end="")
+                print(chr(out), end="")
         return ilast(self.interpretor.run(self.program))
 
 
 def part1(program):
     """Solve for the answer to part 1."""
     script = [
-        # J = not(A) or not(B) or not(C)
-        "NOT A J",
-        "NOT B T",
-        "OR T J",
-        "NOT C T",
-        "OR T J",
-        # J = D and (not(A) or not(B) or not(C))
-        "NOT D T",
-        "NOT T T",
-        "AND T J",
+        "NOT A J",  # J=~A
+        "NOT B T",  # T=~B
+        "OR T J",  # J=~A|~B
+        "NOT C T",  # T=~C
+        "OR T J",  # J=~A|~B|~C
+        "NOT D T",  # T=~D
+        "NOT T T",  # T=D
+        "AND T J",  # J=D&(~A|~B|~C)
     ]
     return Droid(program).exec(script)
 
 
 def part2(program):
     """Solve for the answer to part 2."""
+    script = [
+        "NOT A J",  # J=~A
+        "NOT B T",  # T=~B
+        "OR T J",  # J=~A|~B
+        "NOT C T",  # T=~C
+        "OR T J",  # J=~A|~B|~C
+        "NOT D T",  # T=~D
+        "NOT T T",  # T=D
+        "AND T J",  # J=D&(~A|~B|~C)
+        "NOT E T",  # T=~E
+        "NOT T T",  # T=E
+        "OR H T",  # T=H|E
+        "AND T J",  # J=D&(H|E)&(~A|~B|~C)
+    ]
+    return Droid(program).exec(script, run=True)
