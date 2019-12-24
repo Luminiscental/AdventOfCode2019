703b5f56c7ec40595be41530c80121fd44e95206
Luminiscental <luminiscental01@gmail.com>
2019-12-24 21:48:04 +0000

    Cleanup day 24

--- day24.py
+++ day24.py
@@ -1,23 +1,19 @@
 """AdventOfCode2019 - Day 24"""
-import math
-from util import adjacent_2d_tuples, tuple_add, tuple_sub, tuple_scale, sign
+from util import adjacent_2d_tuples
 
+# tile: set(tiles adjacent to tile in inner layer)
+ADJACENT_INNER = {
+    (1, 2): {(0, idx) for idx in range(5)},
+    (3, 2): {(4, idx) for idx in range(5)},
+    (2, 1): {(idx, 0) for idx in range(5)},
+    (2, 3): {(idx, 4) for idx in range(5)},
+}
 
-def alive(tile, board):
-    """Check if a tile is alive given the board state"""
-    return board[tile[0] + 5 * tile[1]]
-
-
-def display_board(board):
-    """Return a human readable string for the board state."""
-    return "\n".join(
-        "".join("#" if alive((x, y), board) else "." for x in range(5))
-        for y in range(5)
-    )
-
-
-def display_layers(layers):
-    return "\n\n".join(display_board(layer) for layer in layers)
+# tile: set(tiles adjacent to tile in outer layer)
+ADJACENT_OUTER = {
+    outer: set(inner for inner in ADJACENT_INNER if outer in ADJACENT_INNER[inner])
+    for outer in set.union(*ADJACENT_INNER.values())
+}
 
 
 def valid(tile):
@@ -25,54 +21,24 @@ def valid(tile):
     return tile[0] in range(5) and tile[1] in range(5)
 
 
+def alive(tile, board):
+    """Check if a tile is alive given the board state"""
+    return valid(tile) and board[tile[0] + 5 * tile[1]]
+
+
 def count_neighbours(board, tile):
     """Count the neighbouring bugs to a tile."""
-    return sum(
-        valid(neighbour) and alive(neighbour, board)
-        for neighbour in adjacent_2d_tuples(tile)
-    )
+    return sum(alive(neighbour, board) for neighbour in adjacent_2d_tuples(tile))
 
 
-def middle_bugs(board, direction):
-    """Count the bugs neighbouring the middle in a given direction."""
-    offsets = [direction] if 0 in direction else [(direction[0], 0), (0, direction[1])]
-    return sum(alive(tuple_add((2, 2), offset), board) for offset in offsets)
-
-
-def edge_bugs(board, direction):
-    """Count the bugs on the edge in a given direction."""
-    # Assumes direction is 0 in one component
-    if direction[0] == 0:
-        center_line = ((idx, 2) for idx in range(5))
-    else:
-        center_line = ((2, idx) for idx in range(5))
-    tiles = (
-        tuple_add(center_point, tuple_scale(direction, 2))
-        for center_point in center_line
-    )
-    return sum(alive(tile, board) for tile in tiles)
-
-
-def layered_neighbours(tile, layer, inner=None, outer=None):
-    """Count the neighbouring bugs to a tile on a layer given the inner and outer layers,
-    or None if they are empty.
-    """
-    flat_neighbours = count_neighbours(layer, tile)
-    tile_offset = tuple_sub(tile, (2, 2))  # offset from center
-    tile_dir = tuple(map(sign, tile_offset))
-    if 2 in tile_offset or -2 in tile_offset:  # outer neighbours
-        if outer is None:
-            adj_neighbours = 0
-        else:
-            adj_neighbours = middle_bugs(
-                outer, tuple(map(lambda x: math.trunc(x / 2), tile_offset))
-            )
-    else:  # inner neighbours
-        if inner is None or 0 not in tile_dir:  # on diagonals there are no adjacents
-            adj_neighbours = 0
-        else:
-            adj_neighbours = edge_bugs(inner, tuple(map(sign, tile_offset)))
-    return flat_neighbours + adj_neighbours
+def count_layered_neighbours(layer, inner, outer, tile):
+    """Count the neighbouring bugs to a tile including from neighbouring layers."""
+    total = count_neighbours(layer, tile)
+    if inner is not None and tile in ADJACENT_INNER:
+        total += sum(alive(adj, inner) for adj in ADJACENT_INNER[tile])
+    if outer is not None and tile in ADJACENT_OUTER:
+        total += sum(alive(adj, outer) for adj in ADJACENT_OUTER[tile])
+    return total
 
 
 def alive_next(bug, neighbours):
@@ -84,42 +50,40 @@ def alive_next(bug, neighbours):
     return bug
 
 
-def step(board):
+def step_board(board):
     """Return the next board state."""
     return tuple(
-        alive_next(bug, count_neighbours(board, (idx % 5, idx // 5)))
-        for idx, bug in enumerate(board)
+        alive_next(alive((x, y), board), count_neighbours(board, (x, y)))
+        for y in range(5)
+        for x in range(5)
     )
 
 
-def step_layer(layer, inner=None, outer=None):
-    """Step a layer state given its inner and outer layers, None if they are empty."""
+def step_layer(layer, inner, outer):
+    """Return the next state of a layer given its outer and inner neighbour layers."""
     return tuple(
-        idx != 12
+        (x, y) != (2, 2)  # ignore the center tile
         and alive_next(
-            bug, layered_neighbours((idx % 5, idx // 5), layer, inner, outer)
+            alive((x, y), layer), count_layered_neighbours(layer, inner, outer, (x, y))
         )
-        for idx, bug in enumerate(layer)
+        for y in range(5)
+        for x in range(5)
     )
 
 
 def step_layers(layers):
-    """Return the next layer states, modifies layers but leaves it in an equivalent state."""
-    # Add blank layers to expand into
-    layers.insert(0, [False] * 5 * 5)
+   # flat neighbours   """Return the next layer states."""
+    # Add padding for bugs to expand into
     layers.append([False] * 5 * 5)
-    # Step layers in the middle
-    result = [
+    layers.insert(0, [False] * 5 * 5)
+    # Step all the layers
+    result_middle = [
         step_layer(layer, inner, outer)
         for outer, layer, inner in zip(layers, layers[1:], layers[2:])
     ]
-    # Step the layers on either end
-    result.insert(0, step_layer(layers[0], inner=layers[1], outer=None))
-    result.append(step_layer(layers[-1], inner=None, outer=layers[-2]))
-    # If the inner layer wasn't expanded into leave it out
-    if not any(result[-1]):
-        result.pop()
-    return result
+    result_start = [step_layer(layers[0], inner=layers[1], outer=None)]
+    result_end = [step_layer(layers[-1], inner=None, outer=layers[-2])]
+    return result_start + result_middle + result_end
 
 
 def biodiversity(board):
@@ -127,7 +91,7 @@ def biodiversity(board):
     return sum(2 ** idx if bug else 0 for idx, bug in enumerate(board))
 
 
-def count_bugs(layers):
+def bug_count(layers):
     """Count the number of bugs given the layer states."""
     return sum(bug for layer in layers for bug in layer)
 
@@ -147,7 +111,7 @@ def part1(initial_state):
     state = initial_state
     while state not in states:
         states.add(state)
-        state = step(state)
+        state = step_board(state)
     return biodiversity(state)
 
 
@@ -156,4 +120,4 @@ def part2(initial_state):
     layers = [initial_state]
     for _ in range(200):
         layers = step_layers(layers)
-    return count_bugs(layers)
+    return bug_count(layers)
